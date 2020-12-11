91c67bd898103c9e2e1227880bf286cbf4f3d46c
Allan Cedric <allan.py3000@gmail.com>
2020-12-11 09:52:22 -0300

    Trabalho de Alg3 finalizado

---
+++ myavl.c
@@ -0,0 +1,44 @@
+#include "ArvoreAVL/avl.h"
+#define _SUCESSOR_
+
+/* Impressão emOrdem da árvore AVL */
+void printInOrder(AVL *root, int level);
+
+int main()
+{
+    /* Inicialização da árvore AVL */
+    AVL *rootAVL = createAVL();
+
+    /* Inputs na árvore */
+    char opInput;
+    int keyInput;
+    while (!feof(stdin))
+    {
+        fscanf(stdin, "%c %i\n", &opInput, &keyInput);
+        if (opInput == 'i')
+            rootAVL = insertNodeAVL(rootAVL, keyInput);
+        else if (opInput == 'r')
+            rootAVL = removeNodeAVL(rootAVL, keyInput);
+        else
+            fprintf(stdout, "Operação inválida\n");
+    }
+
+    /* Output esperado */
+    printInOrder(rootAVL, -1);
+
+    rootAVL = destroyAVL(rootAVL);
+    rootAVL = NULL;
+
+    return 0;
+}
+
+void printInOrder(AVL *root, int level)
+{
+    if (root)
+    {
+        level++;
+        printInOrder(root->left, level);
+        fprintf(stdout, "%i,%i\n", root->key, level);
+        printInOrder(root->right, level);
+    }
+}
